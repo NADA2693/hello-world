bbc367a8984d5e529973ba28844963bc2de04700
NADA2693 <165019474+NADA2693@users.noreply.github.com>
2024-03-26 08:27:27 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    basic.showString("ola, tudo bem com voce")
+    basic.showIcon(IconNames.Asleep)
+    basic.showString("meu nome e ana")
+    basic.showString("qual e o seu nome")
+})
